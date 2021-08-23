f45f218d48955f3819ed366f5f22a4af81b577c4
David Nix <hello@davidnix.io>
2021-08-22 18:36:48 -0600

    remove unused formatter

--- presenter/format.go
+++ presenter/format.go
@@ -3,8 +3,6 @@ package presenter
 import (
 	"fmt"
 	"strconv"
-
-	"github.com/fatih/color"
 )
 
 type Float float64
@@ -14,17 +12,3 @@ func (f Float) String() string { return fmt.Sprintf("%.1f", f) }
 type Int int
 
 func (i Int) String() string { return strconv.Itoa(int(i)) }
-
-type Injury string
-
-func (injury Injury) String() string {
-	i := string(injury)
-	switch i {
-	case "":
-		return i
-	case "Q":
-		return color.YellowString(i)
-	default:
-		return color.RedString(i)
-	}
-}
